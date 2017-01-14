d06b1740f7d4b77c45b649d2c3c195a9b280f470
hyunjik.bae <hyunjik.bae@nettention.com>
2017-01-14 19:52:14 +0900

    Add project files.

---
+++ WhoHasMore/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhoHasMore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int s1 = 0;
+        int s2 = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s1++;
+            button1.Text = "배태환 : " + s1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s2++;
+            button2.Text = "배지환 : " + s2;
+
+        }
+    }
+}
